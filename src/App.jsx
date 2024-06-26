bba11d4717fbc5384b0f2b61133c9da458b6f07b
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-26 16:59:44 +0100

    Created cake container and added data to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import CakeContainer from './containers/CakeContainer.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Bakery</h1>
+      {/* <CakeContainer/> */}
     </>
   )
 }
