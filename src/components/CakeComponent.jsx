4b67f4e37494272b96ff0e4b126c1f2a1c1170c1
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-26 17:45:31 +0100

    implemented map to list ingredients

--- src/components/CakeComponent.jsx
+++ src/components/CakeComponent.jsx
@@ -3,18 +3,20 @@
 const CakeComponent = ({name, ingredients, price, rating}) => {
 
     const handleIngredients = (ingredientsArray) => {
-        
+        ingredientsArray.forEach(ingredient => {
+            return (
+                <p>{ingredient}</p>
+            )
+        })
     }
 
     return (
         <>
             <h2>{name}</h2>
-            <div>{ingredients}</div>
+            <div>{ingredients.map(item => <p>{item}</p>)}</div>
             <p>Price: £{price}</p>
             <p>Rating: {rating}</p>
         </>
-
-
     )
 
 }
